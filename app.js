2ae8db66976ea3eeda47ed532b5c5cc78473900e
Sushmita Singh <sushmitasingh1899@gemail.com>
2020-07-01 14:48:36 +0530

    Add url and remove databade

--- app.js
+++ app.js
@@ -1,30 +1,62 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const request = require("request");
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
 
 const app = express();
 
-const url = 'mongod://localhost:27017';
-const dbName = 'photos';
-const client = new MongoClient(url);
-client.connect(function(err) {
-	  assert.equal(null, err);
-	  console.log("Connected successfully to server");
-
-	  const db = client.db(dbName);
-
-	  client.close();
-	});
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-
-app.get("/",function(req,res){
-	res.sendFile(__dirname + "/test.html");
+app.get("/",function(req, res){
+	res.sendFile(__dirname + "/testPhoto.html");
+	console.log(req.originalUrl);
+	invokeAPI(req, res);
+	//res.send(result);
 });
+
+app.get("/search",function(req, res){
+	//res.sendFile(__dirname + "/test.html");
+	console.log(req.originalUrl);
+	invokeAPI(req, res);
+	//res.send(result);
+});
+
+function invokeAPI(req, res){
+	console.log(req.body.keywords);
+	var keyword = req.param('keywords');
+	var limit = req.param('limit');
+	var baseURL = "https://unsplash.com/napi/search?query=";
+	var optional = "&xp=&per_page=";
+	var finalURL = baseURL + keyword + optional + limit;
+	console.log(finalURL);
+	
+	request(finalURL,function(error, response, body){
+		if(error){
+			console.log(error);
+			return;
+		}
+		var result = [];
+		//request("https://unsplash.com/napi/search?query=cat&xp=&per_page=2",function(error, response, body){
+		//console.log(response);
+		//console.log(response.statusCode);
+		//console.log(body);
+		var data = JSON.parse(body);
+		var images = data.photos.results;
+		var i;
+		for (i in images) {
+			result.push(images[i].urls);
+			//res.write(result[i]);
+			//var img = req.get('result[i]');
+			//res.write(img + "  ")
+		}
+
+		console.log(result);
+		res.write(JSON.stringify(result));
+		res.send();
+		
+	});
+} 
 
 
 app.listen(3000, function(){
