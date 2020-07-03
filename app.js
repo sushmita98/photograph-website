0c5d8d3af7a656644f448115aecdd2386122a1d1
Sushmita Singh <sushmitasingh1899@gemail.com>
2020-07-03 23:40:02 +0530

    major changes

--- app.js
+++ app.js
@@ -4,31 +4,34 @@ const request = require("request");
 
 const app = express();
 
+
+var keyword;
+var result = [];
+
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
-
+ 
 app.get("/",function(req, res){
-	res.sendFile(__dirname + "/testPhoto.html");
-	console.log(req.originalUrl);
-	invokeAPI(req, res);
-	//res.send(result);
+	res.render("search");
 });
 
 app.get("/search",function(req, res){
-	//res.sendFile(__dirname + "/test.html");
-	console.log(req.originalUrl);
-	invokeAPI(req, res);
-	//res.send(result);
+	res.render("photos",{key: keyword , imgURL: result});
 });
 
-function invokeAPI(req, res){
+/*app.get("/search/quality",function(req, res){
+	res.render("qualityPhotos",{key: keyword , imgURL: finalResult});
+});
+*/
+
+app.post("/",function(req, res){
 	console.log(req.body.keywords);
-	var keyword = req.param('keywords');
-	var limit = req.param('limit');
+	keyword = req.body.keywords;
 	var baseURL = "https://unsplash.com/napi/search?query=";
-	var optional = "&xp=&per_page=";
-	var finalURL = baseURL + keyword + optional + limit;
+	var optional = "&xp=&per_page=20";
+	var finalURL = baseURL + keyword + optional;
 	console.log(finalURL);
 	
 	request(finalURL,function(error, response, body){
@@ -36,29 +39,21 @@ function invokeAPI(req, res){
 			console.log(error);
 			return;
 		}
-		var result = [];
-		//request("https://unsplash.com/napi/search?query=cat&xp=&per_page=2",function(error, response, body){
-		//console.log(response);
-		//console.log(response.statusCode);
-		//console.log(body);
 		var data = JSON.parse(body);
 		var images = data.photos.results;
 		var i;
-		for (i in images) {
-			result.push(images[i].urls);
-			//res.write(result[i]);
-			//var img = req.get('result[i]');
-			//res.write(img + "  ")
-		}
-
-		console.log(result);
-		res.write(JSON.stringify(result));
-		res.send();
+		result = []; 
 		
+		for (i in images) {
+			result.push(images[i].urls.thumb);
+		}
+		console.log(result);
+		res.redirect("/search");
 	});
-} 
+});
 
-
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("Server is running on port 3000.");
 });
+
+
